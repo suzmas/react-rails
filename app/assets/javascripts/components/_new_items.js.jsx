af2438c1e70788843dfd91028d774dc9bd618b5d
Suzan Sucro <suzansucro@gmail.com>
2017-04-09 21:04:10 -0700

    Update submit handlers

--- app/assets/javascripts/components/_new_items.js.jsx
+++ app/assets/javascripts/components/_new_items.js.jsx
@@ -1,10 +1,24 @@
 var NewItem = React.createClass({
+
+  handleClick() {
+    var name = this.refs.name.value;
+    var description = this.refs.description.value;
+    $.ajax({
+      url: '/api/v1/items',
+      type: 'POST',
+      data: { item: { name: name, description: description } },
+      success: (item) => {
+        this.props.handleSubmit(item);
+      }
+    });
+  },
+
   render() {
     return (
       <div>
         <input ref='name' placeholder='Enter name' />
         <input ref='description' placeholder='Enter description' />
-        <button>Submit</button>
+        <button onClick={this.handleClick}>Submit</button>
       </div>
     )
   }
