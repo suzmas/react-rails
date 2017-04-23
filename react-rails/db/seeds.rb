27ff79ed9832f2259487dceaba105e23f3ded40a
sslampa <sslampa@gmail.com>
2017-04-22 23:26:13 -0700

    Add events to seed

--- react-rails/db/seeds.rb
+++ react-rails/db/seeds.rb
@@ -20,3 +20,33 @@ results["results"].each do |obj|
   Place.create!(name: name, latitude: lat, longitude: lng)
   p "Added #{name}! at #{lat} / #{lng}"
 end
+
+def make_menu
+  menu = Hash.new
+  (rand(10) + 5).times do |num|
+    tmp = rand(20)
+    menu["Item #{num}"] = (tmp <= 1) ? nil : tmp
+  end
+  menu
+end
+
+def make_bool
+  (rand(2) == 1) ? true : false
+end
+
+days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+Place.all.each do |place|
+  days.each do |day|
+    r1 = rand(5) + 3
+    r2 = rand(5) + r1
+    place.events.create(
+      name: "Happy Hour",
+      dow: day,
+      start_time: "#{r1}:00",
+      end_time: "#{r2}:00",
+      menu: make_menu(),
+      has_food: make_bool(),
+      has_drink: make_bool(),
+    )
+  end
+end
