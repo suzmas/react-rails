e9f4d4383e273c00a604bf9220ea6b66cf208166
sslampa <sslampa@gmail.com>
2017-04-26 21:27:38 -0700

    Add filter for day, food, drink

--- react-rails/app/controllers/places_controller.rb
+++ react-rails/app/controllers/places_controller.rb
@@ -15,15 +15,29 @@ class PlacesController < ApplicationController
     @places = Place.place(params[:place].downcase).pluck(:id)
 
     @places = @places.map do |place|
+      @events = Event.where(place_id: place)
+
+      if !params[:day].nil?
+        @events = @events.merge(Event.day(params[:day]))
+      end
+
+      if !params[:food].nil?
+        @events = @events.merge(Event.food(params[:food]))
+      end
+
+      if !params[:drink].nil?
+        @events = @events.merge(Event.drink(params[:drink]))
+      end
       {
         Place.find(place).name => {
           places: Place.find(place),
-          events: Event.where(place_id: place).day(params[:day])
+          events: @events
+          # events: Event.where(place_id: place).day(params[:day]).food(params[:food]).drink(params[:drink])
         }
       }
     end
 
-    render json: @places
+    render json: JSON.pretty_generate(JSON.parse(@places.to_json))
   end
 
   def place
--- react-rails/app/models/event.rb
+++ react-rails/app/models/event.rb
@@ -2,4 +2,6 @@ class Event < ApplicationRecord
   belongs_to :place
 
   scope :day, -> (day) { where("LOWER(events.dow) = ?", "#{day}") }
+  scope :food, -> (food) { where("events.has_food = ?", "#{food}") }
+  scope :drink, -> (drink) { where("events.has_drink = ?", "#{drink}") }
 end
