84a14810fccfe1aec1216a2f8478b26c2429cd9c
Suzan Sucro <suzansucro@gmail.com>
2017-05-10 14:15:57 -0700

    Schema.rb config and version control. Placemap resize

--- react-rails/config/application.rb
+++ react-rails/config/application.rb
@@ -10,7 +10,8 @@ module ReactRails
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # -- all .rb files in that directory are automatically loaded
+    config.active_record.schema_format = :ruby
     config.active_record.time_zone_aware_types = [:datetime]
   end
 end
