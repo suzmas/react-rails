30390c49355f235685b838bf00ccef7545136227
sslampa <sslampa@gmail.com>
2017-06-05 23:53:19 -0700

    Fix map console error

--- client/app/bundles/Places/components/Map.jsx
+++ client/app/bundles/Places/components/Map.jsx
@@ -38,9 +38,13 @@ export default class PlaceMap extends React.Component {
 
     const bounds = latLngBounds()
 
-    places
-      .map(place => { return [place.place.latitude, place.place.longitude] })
-        .map(data => { bounds.extend(data) })
+    if (places.length > 0) {
+      places
+        .map(place => { return [place.place.latitude, place.place.longitude] })
+          .map(data => { bounds.extend(data) })
+    } else {
+      bounds.extend([[32.7057, -117.1611], [32.7557, -117.1611]])
+    }
 
     // control for error w/ identical LatLngBounds
     if (places.length === 1) {
