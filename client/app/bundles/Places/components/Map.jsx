46e33af379e76aaf87733a8c1a11af1eea21b311
sslampa <sslampa@gmail.com>
2017-06-07 22:45:26 -0700

    Refactor code to generalize functions to include Place/Events

--- client/app/bundles/Places/components/Map.jsx
+++ client/app/bundles/Places/components/Map.jsx
@@ -12,69 +12,45 @@ export default class PlaceMap extends React.Component {
   }
 
   placeMarker = () => {
-    let places = this.props.data
+    let places = (this.props.view === "place") ?
+      this.props.data.map(place => {return place.place}) :
+      this.props.allEvents
 
     if (places.length < 1) return
 
     let list = places.map(place => {
       let zIndexOffset = 0
       let iconUrl = "assets/inactive.png"
-      if (this.props.selected === place.place.id) {
+      if (this.props.selected === place.id) {
         zIndexOffset = 1000; iconUrl = "assets/active.png"
       }
-      const position = [place.place.latitude, place.place.longitude]
+      const position = [place.latitude, place.longitude]
       const icon = L.icon({iconUrl: iconUrl, iconSize: 35})
 
       return (
-        <Marker key={place.place.id}
+        <Marker key={place.id}
           position={position}
           zIndexOffset={zIndexOffset}
           icon={icon}>
           <Popup>
-            <span>{place.place.name}</span>
+            <span>{place.name}</span>
           </Popup>
         </Marker>
       )
     })
-    return list
-  }
-
-  placeEventMarker = () => {
-    let places = this.props.allEvents
-
-    if (places.length < 1) return
-
-    let list = places.map(event => {
-      let zIndexOffset = 0
-      let iconUrl = "assets/inactive.png"
-      if (this.props.selected === event.id) {
-        zIndexOffset = 1000; iconUrl = "assets/active.png"
-      }
-      const position = [event.latitude, event.longitude]
-      const icon = L.icon({iconUrl: iconUrl, iconSize: 35})
-
-      return (
-        <Marker key={event.id}
-          position={position}
-          zIndexOffset={zIndexOffset}
-          icon={icon}>
-          <Popup>
-            <span>{event.name}</span>
-          </Popup>
-        </Marker>
-      )
-    })
-
     return list
   }
 
   getCoords = () => {
-    let places = this.props.data
+    let places = (this.props.view === "place") ?
+      this.props.data.map(place => {return place.place}) :
+      this.props.allEvents
+
     const bounds = latLngBounds()
 
     if (places.length > 0) {
       places
-        .map(place => { return [place.place.latitude, place.place.longitude] })
+        .map(place => { return [place.latitude, place.longitude] })
           .map(data => { bounds.extend(data) })
     } else {
       bounds.extend([[32.7057, -117.1611], [32.7557, -117.1611]])
@@ -87,32 +63,14 @@ export default class PlaceMap extends React.Component {
     return bounds
   }
 
-  getEventCoords = () => {
-    let places = this.props.allEvents
-    const bounds = latLngBounds()
-
-    if (places.length > 0) {
-      places.map(event => { return [event.latitude, event.longitude] }).map(data => { bounds.extend(data) })
-    } else {
-      bounds.extend([[32.7057, -117.1611], [32.7557, -117.1611]])
-    }
-
-    if (places.length === 1) {
-      bounds._northEast.lat += .001
-    }
-
-    return bounds
-  }
-
   render() {
-    const bounds = (this.props.view === "place") ? this.getCoords() : this.getEventCoords()
-    const marker = (this.props.view === "place") ? this.placeMarker() : this.placeEventMarker()
+    const bounds = this.getCoords()
     return (
       <Map bounds={bounds}>
         <TileLayer
           attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
           url='http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png' />
-          {marker}
+          {this.placeMarker()}
       </Map>
     )
   }
