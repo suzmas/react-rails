bda274814d12f8f8a74d75100946d992ee11fced
Stephen Lampa <sslampa@gmail.com>
2017-06-10 03:26:10 -0700

    Change seed to have a more accurate food/drink menu

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,20 +26,41 @@ when "development"
     p "Added #{name}! at #{lat} / #{lng}"
   end
 
-  def make_menu
-    names = ["pizza", "burger", "red wine", "wings", "IPA", "whiskey", "vodka",
-      "tequila", "fries", "mimosa", "fish", "appetizer", "steak", "salad", "broccoli",
-      "avocado", "burrito", "taco", "Coors", "Mickey's", "sour"]
+  def make_menu(bool_food, bool_drink)
+    food = ["meat mouth pizza", "chorizo burger", "garlic parmesan wings", "french fries", "baked salmon", "new york strip steak",
+      "cobb salad", "greek salad", "california burrito", "shrimp cocktail",
+      "2 adobada tacos", "Suzan's Special", "bacon omelette", "the works bagel"]
+    drink = ["red wine", "white wine", "whiskey", "vodka", "tequila", "IPA",
+      "lager", "sour", "Bud Light", "Natty Ice", "mimosa", "champagne", "7&7",
+      "fireball", "soju", "gin", "sake"]
+
+    combined = []
     menu = Hash.new
+    if (bool_food)
+      tmp = rand(20)
+      combined += food
+      menu["#{food[rand(food.length)]}"] = (tmp <= 1) ? nil : tmp
+    end
+
+    if (bool_drink)
+      tmp = rand(20)
+      combined += drink
+      menu["#{drink[rand(drink.length)]}"] = (tmp <= 1) ? nil : tmp
+    end
+
     (rand(10) + 5).times do |num|
       tmp = rand(20)
-      menu["#{names[rand(names.length)]}"] = (tmp <= 1) ? nil : tmp
+      menu["#{combined[rand(combined.length)]}"] = (tmp <= 1) ? nil : tmp
     end
     menu
   end
 
-  def make_bool
+  def make_bool_food
     (rand(2) == 1) ? true : false
+  end
+
+  def make_bool_drink(food)
+    (food) ? ((rand(2) == 1) ? true : false) : true
   end
 
   days = [
@@ -57,7 +78,8 @@ when "development"
 
   Place.all.each_with_index do |place, index|
     place_names = ["Pizza Time", "Burrito Time", "Taco Time", "Fiesta Time",
-      "Sushi Time", "Beer Time", "Shots Time", "Get Down Time", "Help Me Out Time"
+      "Sushi Time", "Beer Time", "Shots Time", "Get Down Time", "Help Me Out Time",
+      "Suzan's Time", "Hammer Time", "Big Time Rush", "Fresh Out of Thyme"
     ]
     day = rand(days.length)
     days[day].each do |value|
@@ -67,14 +89,17 @@ when "development"
         break if r2 < 24
       end
 
+      food = make_bool_food()
+      drink = make_bool_drink(food)
+
       place.events.create!(
         name: "#{place_names[rand(place_names.length)]}",
         dow: value,
         start_time: "#{r1}:00",
         end_time: "#{r2}:00",
-        menu: make_menu(),
-        has_food: make_bool(),
-        has_drink: make_bool(),
+        menu: make_menu(food, drink),
+        has_food: food,
+        has_drink: drink,
       )
     end
   end
