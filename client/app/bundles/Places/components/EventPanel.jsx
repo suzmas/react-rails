acd1be3ece2e76489cf1ea88b11f2a8f7a9279b9
Suzan Sucro <suzansucro@gmail.com>
2017-06-15 21:35:46 -0700

    Add address to map marker popup

--- client/app/bundles/Places/components/EventPanel.jsx
+++ client/app/bundles/Places/components/EventPanel.jsx
@@ -20,6 +20,7 @@ export default class EventPanel extends React.Component {
 
   // May need to find a way to tie in place and event together in this panel
   eventPanel = (e) => {
+    console.log(e)
     const headerString = (
       <div style={{fontSize: "12px"}}>
         <h4>{e.name}</h4>
